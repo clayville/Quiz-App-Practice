c5f614c7d1565c96cb5f984c82ed323f653aa1f4
Clayvile N <xyclayville@gmail.com>
2022-09-22 15:29:48 +0200

    Initial commit

--- Quiz App/QuizVC.swift
+++ Quiz App/QuizVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  QuizVC.swift
 //  Quiz App
 //
 //  Created by Clayville on 22/9/2022.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class QuizVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
